a062a55a3f494fd47bb1326a75a45f94ee7c2784
Trần Lâm <122392761+hoshimiyo@users.noreply.github.com>
2026-07-27 09:22:45 +0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++){
+            int product = 1;
+            for(int j = 0; j < n; j++){
+                if(j != i){
+                    product = product * nums[j];
+                }
+            }
+            res[i] = product;
+        }
+
+        return res;
+    }
+}
