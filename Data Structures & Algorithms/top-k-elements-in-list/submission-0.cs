9a280f3f444bf52f75cfe2a1aed7c2e7deb2bc7b
Trần Lâm <122392761+hoshimiyo@users.noreply.github.com>
2026-08-25 23:36:38 +0700

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.cs
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cs
@@ -1,25 +1,16 @@
 public class Solution {
     public int[] TopKFrequent(int[] nums, int k) {
-        var count = new Dictionary<int, int>();
-        foreach (var num in nums) {
-            if (count.ContainsKey(num)) {
-                count[num]++;
-            } else {
-                count[num] = 1;
-            }
+        Dictionary<int, int> count = new Dictionary<int, int>();
+        foreach(int num in nums){
+            if (count.ContainsKey(num)) count[num]++;
+            else count[num] = 1;
         }
 
-        var heap = new PriorityQueue<int, int>();
-        foreach (var entry in count) {
-            heap.Enqueue(entry.Key, entry.Value);
-            if (heap.Count > k) {
-                heap.Dequeue();
-            }
-        }
-
-        var res = new int[k];
-        for (int i = 0; i < k; i++) {
-            res[i] = heap.Dequeue();
+        List<int[]> arr = count.Select(entry => new int[]{entry.Value, entry.Key}).ToList();
+        arr.Sort((a, b) => b[0].CompareTo(a[0]));
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++){
+            res[i] = arr[i][1];
         }
         return res;
     }
