74814d3fca4eb90c818abf2aa9cbb5fd850d4173
Trần Lâm <122392761+hoshimiyo@users.noreply.github.com>
2026-08-26 17:34:08 +0700

    Update: products-of-array-discluding-self - submission-1

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cs
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cs
@@ -1,26 +1,31 @@
 public class Solution {
-    public int[] ProductExceptSelf(int[] nums) {        
-        int product = 1, zeroCount = 0;
+    public int[] ProductExceptSelf(int[] nums) {
+        int zeroCount = 0;
+        int prod = 1;
         for(int i = 0; i < nums.Length; i++){
-            if(nums[i] != 0)    
-                product = product * nums[i];
-            else
+            if(nums[i] == 0){
                 zeroCount++;
+            }
+            else {
+                prod *= nums[i];
+            }
         }
-
         if(zeroCount > 1){
             return new int[nums.Length];
         }
-
         int[] res = new int[nums.Length];
         for(int i = 0; i < nums.Length; i++){
-            if(zeroCount > 0){
-                res[i] = (nums[i] == 0) ? product : 0;
-            } else {
-                res[i] = product / nums[i];
+            if(zeroCount == 1){
+                if(nums[i] == 0){
+                    res[i] = prod;
+                } else {
+                    res[i] = 0;
+                }
+            }
+            if(zeroCount == 0){
+                res[i] = prod / nums[i];
             }
         }
-
         return res;
     }
 }
