4674ee7ec9f84b7b959f8dd80ec7aabd7facd2a5
Trần Lâm <122392761+hoshimiyo@users.noreply.github.com>
2026-08-26 16:20:00 +0700

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.cs
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cs
@@ -1,50 +1,27 @@
 public class Solution {
 
     public string Encode(IList<string> strs) {
-        if(strs.Count == 0)
-            return "";
-            
-        List<int> sizes = new List<int>();
-        StringBuilder resString = new StringBuilder();
-        foreach(string s in strs)
-        {
-            sizes.Add(s.Length);
+        StringBuilder res = new StringBuilder();
+        foreach(string s in strs){
+            res.Append(s.Length).Append('#').Append(s);
         }
-        foreach(int sz in sizes)
-        {
-            resString.Append(sz).Append(',');
-        }
-        resString.Append('#');
-        foreach(string s in strs)
-        {
-            resString.Append(s);
-        }
-        return resString.ToString();
+        return res.ToString();
     }
 
     public List<string> Decode(string s) {
-        if(s.Length == 0)
-        {
-            return new List<string>();
-        }
-        List<int> sizes = new List<int>();
-        List<string> resString = new List<string>();
+        List<string> res = new List<string>();
         int i = 0;
-        while(s[i] != '#')
-        {
+        while(i < s.Length){
             int j = i;
-            while(s[j] != ',')
-            {
+            while(s[j] != '#'){
                 j++;
             }
-            sizes.Add(int.Parse(s.Substring(i, j - i)));
+            int length = int.Parse(s.Substring(i, j - i));
             i = j + 1;
+            j = i + length;
+            res.Add(s.Substring(i, length));
+            i = j;
         }
-        i++;
-        foreach (int sz in sizes) {
-            resString.Add(s.Substring(i, sz));
-            i += sz;
-        }
-        return resString;
+        return res;
    }
 }
