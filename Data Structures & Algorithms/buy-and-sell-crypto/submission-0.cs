a686f847888a4e1ffb889874f6f60445ed1c839f
Trần Lâm <122392761+hoshimiyo@users.noreply.github.com>
2026-08-27 16:38:02 +0700

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -1,16 +1,16 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
         int l = 0, r = 1;
-        int max = 0;
+        int res = 0;
         while(r < prices.Length){
-            if(prices[r] > prices[l]){
-                int profit = prices[r] - prices[l];
-                max = Math.Max(max, profit);
-            }else{
+            if(prices[l] < prices[r]){
+                int diff = prices[r] - prices[l];
+                res = Math.Max(res, diff);
+            } else {
                 l = r;
             }
-            r++;
-        } 
-        return max;
+            r++;            
+        }
+        return res;
     }
 }
